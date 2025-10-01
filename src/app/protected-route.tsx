472758a57dc97afccca3771613b50167f8e20555
thefomin <raveyt@yandex.ru>
2025-10-01 16:36:18 +0700

    feat: protected route added

---
+++ src/app/protected-route.tsx
@@ -0,0 +1,28 @@
+
+import { Outlet, redirect } from "react-router-dom";
+
+import { Navigate } from "react-router-dom";
+import { enableMocking } from "@/shared/api/mocks";
+import { ROUTES, useSession } from "@/shared/config";
+
+export function ProtectedRoute() {
+  const { session } = useSession();
+
+  if (!session) {
+    return <Navigate to={ROUTES.LOGIN} />;
+  }
+
+  return <Outlet />;
+}
+
+export async function protectedLoader() {
+  await enableMocking();
+
+  const token = await useSession.getState().refreshToken();
+
+  if (!token) {
+    return redirect(ROUTES.LOGIN);
+  }
+
+  return null;
+}
