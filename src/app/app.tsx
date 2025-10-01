d581cad1a6277295f1909534bccf89f33cbc114f
thefomin <raveyt@yandex.ru>
2025-10-01 17:49:45 +0700

    feat: outlet added

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
 function App() {
-	return <></>
+	return (
+		<div className='flex min-h-screen flex-col'>
+			<Outlet />
+		</div>
+	)
 }
 
 export default App
