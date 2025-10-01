a1283926fe7ec9040d1dfaa8ad335388591622f4
thefomin <raveyt@yandex.ru>
2025-10-01 16:36:00 +0700

    feat: global provider added

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+
+import { queryClient } from "@/shared/api";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
