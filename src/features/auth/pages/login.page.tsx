ed3166a7355bb8c2b47810db284e80f758801c0d
thefomin <raveyt@yandex.ru>
2025-10-01 17:55:30 +0700

    feat: components auth added

--- src/features/auth/pages/login.page.tsx
+++ src/features/auth/pages/login.page.tsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom'
+
+import { ROUTES } from '@/shared/config'
+
+import { AuthLayout, LoginForm } from '../ui'
+
 function LoginPage() {
-	return <>login123</>
+	return (
+		<AuthLayout
+			title='Вход'
+			form={<LoginForm />}
+			description={
+				<>
+					<Link
+						to={ROUTES.REGISTER}
+						className='text-lg text-[#41A1E6]'
+					>
+						или создать аккаунт
+					</Link>
+				</>
+			}
+		/>
+	)
 }
 
 export const Component = LoginPage
--- src/features/auth/pages/register.page.tsx
+++ src/features/auth/pages/register.page.tsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom'
+
+import { ROUTES } from '@/shared/config'
+
+import { AuthLayout, RegisterForm } from '../ui'
+
 function RegisterPage() {
-	return <>register</>
+	return (
+		<AuthLayout
+			title='Регистрация'
+			description={
+				<>
+					<Link to={ROUTES.LOGIN} className='text-lg text-[#41A1E6]'>
+						или войти в аккаунт
+					</Link>
+				</>
+			}
+			form={<RegisterForm />}
+		/>
+	)
 }
 
 export const Component = RegisterPage
